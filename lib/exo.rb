f66e2dba9a391d7234886009b2608af89b301a01
Jason Webster <jason@metalabdesign.com>
2013-07-08 10:23:24 -0700

    Register asset paths directly with Sprockets if not in Rails
    Allows usage with anything that uses Sprockets, such as middleman,
    sinatra, etc.

--- lib/exo.rb
+++ lib/exo.rb
@@ -1,8 +1,12 @@
 require "exo/version"
 
 module Exo
-  module Rails
-    class Engine < ::Rails::Engine
-    end
+  if defined? ::Rails::Engine
+    class Rails::Engine < ::Rails::Engine; end;
+  elsif defined? ::Sprockets
+    root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+    ::Sprockets.append_path File.join(root_dir, "vendor", "assets", "javascripts")
+    ::Sprockets.append_path File.join(root_dir, "app", "assets", "javascripts")
+    ::Sprockets.append_path File.join(root_dir, "app", "assets", "stylesheets")
   end
 end

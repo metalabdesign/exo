7c79f4938a0f8ff816e6955372997705e32c97af
Matt Vague <mattvague@gmail.com>
2013-07-11 18:52:21 -0700

    Fix SuperclassMismatch exception

--- lib/exo.rb
+++ lib/exo.rb
@@ -2,7 +2,7 @@ require "exo/version"
 
 module Exo
   if defined? ::Rails::Engine
-    class Rails::Engine < ::Rails::Engine; end;
+    class Engine < ::Rails::Engine; end;
   elsif defined? ::Sprockets
     root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
     ::Sprockets.append_path File.join(root_dir, "vendor", "assets", "javascripts")
